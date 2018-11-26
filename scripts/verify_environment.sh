fc3fce1112418a3cab89e2b0bb3e708381efc010
evasif <evasiif@gmail.com>
2018-11-26 13:37:25 +0000

    display date and time and information on what it does

--- scripts/verify_environment.sh
+++ scripts/verify_environment.sh
@@ -1,3 +1,25 @@
+# Time when the script starts
+echo "Start: $(date)"
+echo ""
+
 # A welcome message that includes the current user’s username
 echo "Welcome $(whoami)"
+echo ""
 
+# Information on what it does
+echo "This is a bash script that checks required programs/dependencies...."
+echo ""
+
+# What type of Linux system it is running on
+uname -a
+echo ""
+
+# Time when the script starts and ends 
+
+
+# Checking versions of all tools
+
+# Generate a log file and output to terminal
+
+# Time when the script ends 
+echo "End: $(date)"
