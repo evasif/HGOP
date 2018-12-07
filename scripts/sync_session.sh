4bb39780cbb4a864f524a9aba00bd3b03224d59f
Isabella Finnsdottir <isabellayr@Isabellas-MacBook-Pro.local>
2018-12-07 10:56:18 +0000

    Still fixing deployment

--- scripts/sync_session.sh
+++ scripts/sync_session.sh
@@ -5,3 +5,8 @@ JENKINS_URL=ec2-52-90-206-44.compute-1.amazonaws.com
 scp -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ~/.aws/credentials ubuntu@${JENKINS_URL}:~/credentials
 ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${JENKINS_URL} "sudo mv ~/credentials /var/lib/jenkins/.aws/credentials"
 ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@${JENKINS_URL} "sudo chmod a+r /var/lib/jenkins/.aws/credentials"
+
+
+#scp -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ~/.aws/GameKeyPair.pem ubuntu@ec2-52-90-206-44.compute-1.amazonaws.com:~/GameKeyPair.pem
+#ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@ec2-52-90-206-44.compute-1.amazonaws.com "sudo mv ~/GameKeyPair.pem /var/lib/jenkins/.aws/GameKeyPair.pem"
+#ssh -o StrictHostKeyChecking=no -i "~/.aws/JenkinsAWSKeyPair.pem" ubuntu@ec2-52-90-206-44.compute-1.amazonaws.com "sudo chmod a+r /var/lib/jenkins/.aws/GameKeyPair.pem"
