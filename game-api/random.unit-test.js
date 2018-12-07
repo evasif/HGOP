b25257d0417a2b1dd337e8fde040be26b4ae0dea
evasif <evasiif@gmail.com>
2018-12-07 14:27:08 +0000

    Adding the last unit test for random

--- game-api/random.unit-test.js
+++ game-api/random.unit-test.js
@@ -5,12 +5,19 @@ test('randomInt function returns a number less or equal than max and greater or 
   const min = 1;
   const max = 10;
   expect(randomInt(min, max)).toBeGreaterThanOrEqual(min);
-  expect(randomInt(min, max)).toBeLessThanOrEqual(max);
+  expect(randomInt(min, max)).toBeLessThan(max);
 });
 
 test('randomInt function returns 0 if min and max are 0', () => {
   const {randomInt} = randomConstructor();
   const min = 0;
   const max = 0;
+  expect(randomInt(min, max)).toEqual(0);
+});
+
+test('randomInt function with min 0 and max 1 should return 0', () => {
+  const {randomInt} = randomConstructor();
+  const min = 0;
+  const max = 1;
   expect(randomInt(min, max)).toEqual(0);
 });
