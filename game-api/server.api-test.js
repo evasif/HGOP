b8ac63111ec186427a7c1984748449f6db04eed4
Isabella Finnsdottir <isabellayr@Isabellas-MacBook-Pro.local>
2018-12-10 13:58:28 +0000

    Change Dockerfile to make it run faster, and create tests for server

---
+++ game-api/server.api-test.js
@@ -0,0 +1,12 @@
+const helper = require('./helper.lib-test.js');
+
+const timeout = 30000;
+
+// TODO what does the done parameter do?
+test(
+    'play a game',
+    function(done) {
+      helper.playGame(process.env.API_URL, done);
+    },
+    timeout
+);
