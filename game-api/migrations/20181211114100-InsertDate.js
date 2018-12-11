b087f0aecd1661dfb984c8240e4041588f1ed078
Isabella Finnsdottir <isabellayr@Isabellas-MacBook-Pro.local>
2018-12-11 12:11:04 +0000

    Fix eslint errors

--- game-api/migrations/20181211114100-InsertDate.js
+++ game-api/migrations/20181211114100-InsertDate.js
@@ -1,13 +1,11 @@
 'use strict';
 
-var dbm;
-var type;
-var seed;
+let dbm;
+let type;
+let seed;
 
-/**
-  * We receive the dbmigrate dependency from dbmigrate initially.
-  * This enables us to not have to rely on NODE_PATH.
-  */
+// We receive the dbmigrate dependency from dbmigrate initially.
+// This enables us to not have to rely on NODE_PATH.
 exports.setup = function(options, seedLink) {
   dbm = options.dbmigrate;
   type = dbm.dataType;
@@ -23,5 +21,5 @@ exports.down = function(db) {
 };
 
 exports._meta = {
-  "version": 1
+  version: 1,
 };
