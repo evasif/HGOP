659b09b133aec5367ea646a5da99b2fcb68a24f5
Isabella Finnsdottir <isabellayr@MacBook-Pro.localdomain>
2018-12-11 20:07:43 +0000

    Change docker file to original

--- scripts/docker_compose_up.sh
+++ scripts/docker_compose_up.sh
@@ -1,7 +1,9 @@
 #!/bin/bash
 
 export GIT_COMMIT=$1
-docker-compose down isabellaf16/hgop-api:$GIT_COMMIT 
-docker-compose up isabellaf16/hgop-api:$GIT_COMMIT 
-docker-compose down isabellaf16/hgop-client:$GIT_COMMIT 
-docker-compose up isabellaf16/hgop-client:$GIT_COMMIT 
+docker-compose down  
+docker-compose up 
+#docker-compose down isabellaf16/hgop-api:$GIT_COMMIT 
+#docker-compose up isabellaf16/hgop-api:$GIT_COMMIT 
+#docker-compose down isabellaf16/hgop-client:$GIT_COMMIT 
+#docker-compose up isabellaf16/hgop-client:$GIT_COMMIT 
