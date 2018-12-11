974400b1d614119ae96f31ec6cd7c461e30b8c5f
evasif <evasiif@gmail.com>
2018-12-11 17:45:47 +0000

    Update docker_compose_up.sh

--- scripts/docker_compose_up.sh
+++ scripts/docker_compose_up.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
 export GIT_COMMIT=$1
-docker-compose down isabellaf16/hgop-api:$GIT_COMMIT 
-docker-compose up isabellaf16/hgop-api:$GIT_COMMIT 
-docker-compose down isabellaf16/hgop-client:$GIT_COMMIT 
-docker-compose up isabellaf16/hgop-client:$GIT_COMMIT 
+docker-compose down
+docker-compose up -d
