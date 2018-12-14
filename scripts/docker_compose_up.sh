63df462ed04b226962b8791686759813446189fc
evasif <evasiif@gmail.com>
2018-12-14 13:25:20 +0000

    exporting api_ip in docker compose up

--- scripts/docker_compose_up.sh
+++ scripts/docker_compose_up.sh
@@ -2,15 +2,11 @@
 
 export GIT_COMMIT=$1
 export ENVIRONMENT=$2
-
+export API_IP=$3
 #docker-compose down isabellaf16/hgop-api:$GIT_COMMIT 
 #docker-compose up isabellaf16/hgop-api:$GIT_COMMIT 
 #docker-compose down isabellaf16/hgop-client:$GIT_COMMIT 
 #docker-compose up isabellaf16/hgop-client:$GIT_COMMIT 
-
-GIT_COMMIT=$1
-ENVIRONMENT=$2
-API_IP=$3
 
 docker-compose down
 docker-compose up -d
